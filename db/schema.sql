4f48e5549c3cc891ccc143f91356819ae12a2bab
manko kolya <mankokolya@gmail.com>
2020-09-04 15:10:58 +0300

    2.2. Модель данных - Post.[285211#307009]

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,10 @@ CREATE database job_grabber;
 \c job_grabber
 
 CREATE schema jobs
-    create table rabbit
+    create table sql_ru_jobs
     (
         id bigserial not null primary key,
-        created date not null
+        url text not null,
+        title varchar(300) not null,
+        date date not null
     );
