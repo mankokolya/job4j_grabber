716c5a629ee143d57b451b849ed21ac51d5956ed
manko kolya <mankokolya@gmail.com>
2020-09-07 16:40:46 +0300

    4. Схема таблицы Post[1731#307015]

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,21 @@ CREATE database job_grabber;
 \c job_grabber
 
 CREATE schema jobs
+
     create table sql_ru_jobs
     (
-        id bigserial not null primary key,
-        url text not null,
+        id    bigserial    not null primary key,
+        url   text         not null,
         title varchar(300) not null,
-        date date not null
+        date  date         not null
+    )
+
+    CREATE TABLE post
+    (
+        id          bigserial    not null primary key,
+        name        varchar(500) not null,
+        description text         not null,
+        link        text         not null,
+        created     date         not null,
+        UNIQUE (link)
     );
