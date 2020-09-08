c9c256415c99c338124777f24e1ec3c734389db9
manko kolya <mankokolya@gmail.com>
2020-09-08 09:05:01 +0300

    2.3. Загрузка деталей поста.[285212#307010] Moved methods initialize and getTime from Post to SqlRuParse

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -1,49 +1,36 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.text.ParseException;
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Post {
     private String author;
     private Date creationTime;
     private String description;
     private String title;
-    private final String url;
+    private String url;
 
-    public Post(String url) {
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setUrl(String url) {
         this.url = url;
-        initializePost(url);
     }
 
-    private void initializePost(String url) {
-        try {
-            Document doc = Jsoup.connect(url).get();
-            Element table = doc.select(".msgTable").first();
-            this.title = table.getElementsByAttributeValue("class", "messageHeader").text();
-            Elements author = table.getElementsByAttributeValue("class", "msgBody");
-            this.author = author.first().text().split(" ")[0];
-            this.description = author.last().text();
-            this.creationTime = getTime(table);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
     }
 
-    private Date getTime(Element table) throws ParseException {
-        String dateAndTime = table.getElementsByClass("msgFooter").text();
-        Pattern datePattern = Pattern.compile("\\d{2}\\s\\X[^\\s]*\\s\\d{2},\\s\\d{2}:\\d{2}");
-        Pattern todayOrYesterday = Pattern.compile("\\X[^,]*,\\s\\d{2}:\\d{2}");
-        Matcher matcher = datePattern.matcher(dateAndTime);
-        Matcher matcher1 = todayOrYesterday.matcher(dateAndTime);
-        String date = matcher.find() ? matcher.group() : matcher1.find() ? matcher1.group() : "";
-        return CorrectDateAndTime.convertDateFromString(date);
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     @Override
@@ -55,9 +42,5 @@ public class Post {
                 + "title='" + title + "', " + System.lineSeparator()
                 + "url='" + url + "', " + System.lineSeparator()
                 + '}';
-    }
-
-    public String getTitle() {
-        return title;
     }
 }
--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -2,10 +2,14 @@ package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class SqlRuParse implements Parse {
@@ -43,8 +47,31 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public Post detail(String link) {
-        return new Post(link);
+    public Post detail(String url) {
+        Post post = new Post();
+        try {
+            post.setUrl(url);
+            Document doc = Jsoup.connect(url).get();
+            Element table = doc.select(".msgTable").first();
+            post.setTitle(table.getElementsByAttributeValue("class", "messageHeader").text());
+            Elements author = table.getElementsByAttributeValue("class", "msgBody");
+            post.setAuthor(author.first().text().split(" ")[0]);
+            post.setDescription(author.last().text());
+            post.setCreationTime(getTime(table));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    private static Date getTime(Element table) throws ParseException {
+        String dateAndTime = table.getElementsByClass("msgFooter").text();
+        Pattern datePattern = Pattern.compile("\\d{2}\\s\\X[^\\s]*\\s\\d{2},\\s\\d{2}:\\d{2}");
+        Pattern todayOrYesterday = Pattern.compile("\\X[^,]*,\\s\\d{2}:\\d{2}");
+        Matcher matcher = datePattern.matcher(dateAndTime);
+        Matcher matcher1 = todayOrYesterday.matcher(dateAndTime);
+        String date = matcher.find() ? matcher.group() : matcher1.find() ? matcher1.group() : "";
+        return CorrectDateAndTime.convertDateFromString(date);
     }
 
     private static List<String> getAllUrls(String url, int firstPage, int lastPage) {
