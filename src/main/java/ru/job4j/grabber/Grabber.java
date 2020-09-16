cc0df92daa3bdfb7664154fa2252e6b4556d703b
manko kolya <mankokolya@gmail.com>
2020-09-16 14:30:28 +0300

    7. Web.[289478#307014] added encoding to win1521 when writing to browser

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -6,6 +6,7 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -81,7 +82,7 @@ public class Grabber implements Grab {
                     try (OutputStream out = socket.getOutputStream()) {
                         out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                         for (Post post : store.getAll()) {
-                            out.write(post.toString().getBytes());
+                            out.write(post.toString().getBytes("windows-1251"));
                             out.write(System.lineSeparator().getBytes());
                         }
                     } catch (IOException io) {
